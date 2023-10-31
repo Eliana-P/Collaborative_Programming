656d6cee4357fd26962cd6ad9cb53b6b2a9f7934
Eliana Pritchard <epritch3@terpmail.umd.edu>
2023-10-31 15:54:16 -0400

    added Collaborative_Programming.py

---
+++ Collaborative_Programming.py
@@ -0,0 +1,9 @@
+"""The text file I am adding to the respository for us all to edit. 
+"""
+
+
+def winner(self, health, players):
+    if players == None:
+        print("You've won the game")
+    else:
+        print("you've lost the game")
