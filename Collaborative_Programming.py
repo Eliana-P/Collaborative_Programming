fe9d3a723cb302a69630de7690861306cdb0e066
Tiara Eltrevoog <tiaraeltrevoog@tiaras-mbp.mynetworksettings.com>
2023-10-31 20:27:07 -0400

    added a turn method

--- Collaborative_Programming.py
+++ Collaborative_Programming.py
@@ -1,5 +1,7 @@
 """The text file I am adding to the respository for us all to edit. 
 """
+def turn():
+    ##
 
 
 def winner(self, health, players):
@@ -7,3 +9,5 @@ def winner(self, health, players):
         print("You've won the game")
     else:
         print("you've lost the game")
+
+
